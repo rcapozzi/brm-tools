8002a0f94756eaaaee98bd24aa41c514722424d3
Raymond Capozzi <rcapozzi@gmail.com>
2012-04-06 20:17:22 +0000

    Fix split processing.

--- bin/pinlog-split.rb
+++ bin/pinlog-split.rb
@@ -1,16 +1,19 @@
 #!/usr/bin/env ruby
 
+#E Sun Apr  1 12:25:54 2001  hostname.com  cm:30411  cm_child.c(107):2559 
+new_message = /^[\s\t\0]*[DEW] /
+regexp = /^\0?([DEW])\s+(.*?)\s+(.*?)\s+(\d+)\s+(.*?)\s+(\d+)\s+(.*?).*?:(.*?)\s/
 fh = $stdout
 pid = nil
 files = Hash.new{|hash, key|
-	fname = "xxx_#{key}.pinlog"
+	fname = "xxx_%05d.pinlog" % key.to_i
 	$stderr.puts "## Create file #{fname}"
 	hash[key] = File.new(fname, "w") 
 }
 
 while line = gets
-	if line =~ /^[DEW].*?\w+:(\d+)/
-		fh = files[$1]		
+	if line =~ regexp
+		fh = files[$8]		
 	end
 	fh.puts line
 end
